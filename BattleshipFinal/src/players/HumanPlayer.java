4ad6fdb6c1fac95af7c8adf087cfdc9c55e3e14f
konstantinNovichenko <novichenko.konstantin@gmail.com>
2021-02-27 17:01:53 -0500

    Initial Commit

---
+++ BattleshipFinal/src/players/HumanPlayer.java
@@ -0,0 +1,79 @@
+package players;
+
+import boards.*;
+
+public class HumanPlayer implements Player {
+	
+	private String name;				// name input from the player
+	private int turns;					// number of turns the player took to win/lose the game
+	private int seconds;				// time taken by the player to win/lose the game
+	private PlayerBoard board;			// instance of the player board to retrieve data
+	
+	private Leaderboard leaderboard;	// instance of leaderboard processing and updating the history of games
+	
+	
+	//***************************************CONSTRUCTORS**********************************************************************
+	
+	// default constructor give the player a default name
+	public HumanPlayer() {
+		name = "Player";		
+		board = new PlayerBoard();
+		leaderboard = board.getLeaderBoard();
+	}
+	
+	// constructor setting the name given by the player
+	public HumanPlayer(String n) {
+		name = n;
+		
+		board = new PlayerBoard();
+		leaderboard = board.getLeaderBoard();
+	}
+	
+	//***************************************************ACCESSOR METHODS*******************************************************
+	
+	@Override
+	public void updateLeaderboard() {
+		leaderboard.updateLeaderboard(turns, seconds);
+	}
+	
+	public int getTurns() {
+		return turns;
+	}
+	
+	public int getSeconds() {
+		seconds = (int)board.getCurrentTimeInSec();
+		return seconds;
+	}
+	
+	public PlayerBoard getPlayerBoard() {
+		return board;
+	}
+	
+	public Leaderboard getLeaderBoard()
+	{
+		return leaderboard;
+	}
+	
+	//***************************************************MUTATOR METHODS*******************************************************
+
+	public void setName(String n) {
+		name = n;
+		leaderboard.setPlayerName(n);
+	}
+	
+	public void setTurns() {
+		turns = board.getRoundCounter();
+	}
+	
+	public void setTurns(int t) {
+		turns = t;
+	}
+	
+	public void setSeconds() {
+		seconds = (int)board.getTotalTimeInSec();
+	}
+	
+	public void setSeconds(int s) {
+		seconds = s;
+	}
+}
